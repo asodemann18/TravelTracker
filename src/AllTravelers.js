db52bbcae8030f40bf15628b6fd0f8b874e45c60
Alex Sodemann <asodemann18@gmail.com>
2020-06-05 21:51:09 -0600

    Create method that calculates the cost for each traveler

--- src/AllTravelers.js
+++ src/AllTravelers.js
@@ -7,10 +7,10 @@ class AllTravelers {
     this.destinations = allDestinationsData;
   }
 
-  getPendingTrips(theStatus) {
+  getTrips(theStatus) {
     if (theStatus === 'pending') {
-      let filteredTrips = this.trips.filter(trip => trip.date >= moment().format('YYYY/MM/DD') && trip.status === 'pending').map(trip => {
-        let tripsObj = {};
+      return this.trips.filter(trip => trip.date >= moment().format('YYYY/MM/DD') && trip.status === 'pending').map(trip => {
+        const tripsObj = {};
         tripsObj.name = this.travelers.find(traveler => traveler.id === trip.userID).name;
         tripsObj.date = trip.date;
         tripsObj.duration = trip.duration;
@@ -19,30 +19,62 @@ class AllTravelers {
         tripsObj.status = trip.status
         return tripsObj
       })
-      return filteredTrips;
     } else {
       return this.trips.map(trip => {
-        let tripsObj = {};
+        const tripsObj = {};
         tripsObj.name = this.travelers.find(traveler => traveler.id === trip.userID).name;
         tripsObj.date = trip.date;
         tripsObj.duration = trip.duration;
         tripsObj.travelers = trip.travelers;
         tripsObj.destination = this.destinations.find(dest => dest.id === trip.destinationID).destination;        
-        tripsObj.status = trip.status
-        return tripsObj
+        tripsObj.status = trip.status;
+        return tripsObj;
       })
     }
   }
+
+  calculateIndividualCost() { //and status === approved
+    return this.trips.map(trip => {
+      const tripsObj = {};
+      tripsObj.userID = trip.userID;
+      tripsObj.date = trip.date;
+      tripsObj.lodgingCost = trip.duration * this.destinations.find(dest => dest.id === trip.destinationID).estimatedLodgingCostPerDay;
+      tripsObj.flightCost = trip.travelers * this.destinations.find(dest => dest.id === trip.destinationID).estimatedFlightCostPerPerson;
+      tripsObj.total = tripsObj.lodgingCost + tripsObj.flightCost;
+      tripsObj.fee = tripsObj.total * .10;
+      return tripsObj;
+    });
+  }
+
+  // calculateTotalCost() {
+  //   const currentYearCost = this.trips.filter(trip => moment(trip.date, 'YYYY/MM/DD').format('YYYY') === moment().format('YYYY')).map(trip => {
+  //     const tripsObj = {};
+  //     tripsObj.lodgingCost = 0;
+  //     tripsObj.flightCost = 0;
+  //     tripsObj.total = 0; //lodging + flight
+  //     tripsObj.fee = 0; // total*.10
+  //   })
+  //   //&& trip.status === 'approved')
+  //   currentYearCost
+  //   console.log(currentYearCost)
+  //   return currentYearCost
+  // }
+
 }
 
 // Total income generated this year (should be 10% of user trip cost)
 //return array of object with:
-//lodgingCost total
+
+//Userid
+//date
+//lodgingCost * days
 //flightCost * Travelers
 //total
 //fee (10% of total)
 //////////////////////////////
 //filter for this year - trips
 //calc destinations 
+//find cost for every person in separate func travelCost - name and id
+//
 
 export default AllTravelers;
--- test/AllTravelers-test.js
+++ test/AllTravelers-test.js
@@ -47,7 +47,7 @@ describe('See if the tests are running', function() {
       {name: 'Ham Leadbeater', date: '2020/08/04', duration: 8, travelers: 1, destination: 'Jakarta, Indonesia', status: 'pending'},
       {name: 'Rachael Vaughten', date: '2020/10/04', duration: 18, travelers: 5, destination: 'Stockholm, Sweden', status: 'pending'}
     ]
-    expect(travelers.getPendingTrips('pending')).to.deep.equal(pendingTravelers)
+    expect(travelers.getTrips('pending')).to.deep.equal(pendingTravelers)
   })
 
 
@@ -60,6 +60,86 @@ describe('See if the tests are running', function() {
       {name: 'Tiffy Grout', date: '2020/04/30', duration: 18, travelers: 3, destination: 'Madrid, Spain', status: 'approved'},
       {name: 'Laverna Flawith', date: '2019/06/29', duration: 9, travelers: 3, destination: 'Lima, Peru', status: 'approved'}
     ]
-    expect(travelers.getPendingTrips()).to.deep.equal(pendingTravelers)
+    expect(travelers.getTrips()).to.deep.equal(pendingTravelers)
   })
+
+  it('should show ALL travelers trip info when argument passed is not "pending', () => {
+    const pendingTravelers = [
+      {name: 'Ham Leadbeater', date: '2020/08/04', duration: 8, travelers: 1, destination: 'Jakarta, Indonesia', status: 'pending'},
+      {name: 'Rachael Vaughten', date: '2020/10/04', duration: 18, travelers: 5, destination: 'Stockholm, Sweden', status: 'pending'},
+      {name: 'Sibby Dawidowitsch', date: '2020/05/22', duration: 17, travelers: 4, destination: 'Sydney, Austrailia', status: 'pending'},
+      {name: 'Leila Thebeaud', date: '2020/02/25', duration: 10, travelers: 2, destination: 'Cartagena, Colombia', status: 'approved'},
+      {name: 'Tiffy Grout', date: '2020/04/30', duration: 18, travelers: 3, destination: 'Madrid, Spain', status: 'approved'},
+      {name: 'Laverna Flawith', date: '2019/06/29', duration: 9, travelers: 3, destination: 'Lima, Peru', status: 'approved'}
+    ]
+    expect(travelers.getTrips('xyz')).to.deep.equal(pendingTravelers)
+  })
+
+  it('should calculate cost of each trip for each traveler', () => {
+    const individualTravelCosts = [
+      {
+        userID: 1,
+        date: '2020/08/04',
+        lodgingCost: 560,
+        flightCost: 890,
+        total: 1450,
+        fee: 145
+      },
+      {
+        userID: 2,
+        date: '2020/10/04',
+        lodgingCost: 1800,
+        flightCost: 3900,
+        total: 5700,
+        fee: 570
+      },
+      {
+        userID: 3,
+        date: '2020/05/22',
+        lodgingCost: 2210,
+        flightCost: 3800,
+        total: 6010,
+        fee: 601
+      },
+      {
+        userID: 4,
+        date: '2020/02/25',
+        lodgingCost: 650,
+        flightCost: 700,
+        total: 1350,
+        fee: 135
+      },
+      {
+        userID: 5,
+        date: '2020/04/30',
+        lodgingCost: 2700,
+        flightCost: 1950,
+        total: 4650,
+        fee: 465
+      },
+      {
+        userID: 6,
+        date: '2019/06/29',
+        lodgingCost: 630,
+        flightCost: 1200,
+        total: 1830,
+        fee: 183
+      }
+    ]
+    expect(travelers.calculateIndividualCost()).to.deep.equal(individualTravelCosts)
+   })
+
+   it('should calculate cost of each trip for each traveler even when an argument is passed', () => {
+    expect(travelers.calculateIndividualCost().length).to.equal(6);
+   })
+
+  it.skip('should calculate cost of lodging for the current year', () => {
+    const travelCosts = [{
+      lodgingCost: 7920,
+      flightCost: 11240,
+      total: 19160,
+      fee: 1916
+    }]
+    expect(travelers.calculateTotalCost()).to.deep.equal(travelCosts)
+   })
 });
